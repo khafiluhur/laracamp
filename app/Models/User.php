f2b53d55a5ad57a8a479f607047c2900b8a97a40
khafiluhur <khafiluhur.99@gmail.com>
2022-01-16 09:34:45 +0700

    install and integrate socialite

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,9 @@ class User extends Authenticatable
         'password',
         'avatar',
         'occupation',
-        'is_admin'
+        'is_admin',
+        'email_verified_at'
+
     ];
 
     /**
